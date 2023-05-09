e169b3fbc374f4611642b4c5a0bc81b448c2c2cc
ArielLahiany <ariel.lahiany@gmail.com>
2023-05-10 00:01:11 +0300

    Fixing the naming of the repository.

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,20 +3,18 @@
 # Variables declaration ---------------------------------------------------------------------------------------------- #
 AWS_REGION="eu-central-1"
 NAMESPACE="kaltura"
-REPOSITORY_NAME="kaltura"
+REPOSITORY_NAME="ariellahiany/kaltura"
 SERVICE="summarizer"
 VERSION=`git describe --tags --abbrev=0`
 
 # Docker ------------------------------------------------------------------------------------------------------------- #
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-aws ecr create-repository --repository-name $REPOSITORY_NAME/$SERVICE --image-scanning-configuration scanOnPush=true --region $AWS_REGION
+aws ecr create-repository --repository-name $REPOSITORY_NAME --image-scanning-configuration scanOnPush=true --region $AWS_REGION
 
-docker build --file deployment/docker/Dockerfile --tag $SERVICE:$VERSION .
+docker build --file deployment/docker/Dockerfile --tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:$VERSION .
 
-docker tag $SERVICE:$VERSION $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME/$SERVICE:$VERSION
-
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME/$SERVICE:$VERSION
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:$VERSION
 
 # Elastic Kubernetes Service ----------------------------------------------------------------------------------------- #
 eksctl create cluster --config-file infrastructure/cluster.yaml
